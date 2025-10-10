abd89089fec4ae7564e33854fbdc17bb51465247
ArtoriPendragon <muhammadnurulloh@yahoo.com>
2025-10-10 14:18:53 +0700

    apdet lagi

--- tugasweb/src/App.js
+++ tugasweb/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import './App.css';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Halo, ini halaman pertamaku di React!</h1>
+        <h1 className='warna-merah'>Halo, ini halaman pertamaku di React!</h1>
         <p>Selamat belajar React.</p>
       </header>
     </div>
