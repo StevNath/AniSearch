1206b0fe0b6dc0c8b1b858160b32c1d0400001c5
StevNath <stevennmeliala@gmail.com>
2025-10-11 22:15:06 +0700

    Update about.jsx

--- src/page/about.jsx
+++ src/page/about.jsx
@@ -4,7 +4,16 @@ export default function About() {
   return (
     <Box p={8}>
       <Heading>About Page</Heading>
-      <Text mt={4}>Ini adalah halaman About.</Text>
+      <Text mt={2}>AniSearch adalah aplikasi web untuk mencari informasi tentang anime.</Text>
+      <Text mt={2}>Dibuat dengan React dan Chakra UI.</Text>
+      <Text mt={2}>Sumber data dari Jikan API (MyAnimeList).</Text>
+      <Text mt={2}>Dibuat Oleh:</Text>
+      <Box as="ul" listStyle="disc" pl={4} mt={2}>
+        <li>Joshua Natanael</li>
+        <li>Muhammad Nurulloh</li>
+        <li>Putra Parlindungan</li>
+        <li>Steven Nathanael Meliala</li>
+      </Box>
     </Box>
   );
 }
