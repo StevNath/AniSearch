b1b0f509213212f0aad3d78cf530b9393729badc
JoshuaImanuel <joshuaimanuel37@gmail.com>
2025-12-31 15:15:37 +0700

    Update Navbar: Menghilangkan fitur login

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -165,7 +165,6 @@ const handlePreviewSearch = async (q) => {
             <Link as={RouterLink} to="/" color="white">Home</Link>
             <Link as={RouterLink} to="/schedules" color="white">Schedules</Link>
             <Link as={RouterLink} to="/about" color="white">About</Link>
-            <Link as={RouterLink} to="/login" color="white">Login</Link>
           </HStack>
         </GridItem>
       </Grid>
